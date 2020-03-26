4ba4f7bf39488b2dbaacf44cb82ef288d32e0eca
nikkolasg <nikkolasg@gmail.com>
2020-03-26 14:31:51 +0000

    working test

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ build:
 	cd map-rs && cargo build --release
 	cp map-rs/target/release/libkyberrs.so map-rs/lib/
 	go build -ldflags="-r map-rs/lib" map.go
+
+test:
+	go build -ldflags="-r map-rs/lib" .
+
--- map_test.go
+++ map_test.go
@@ -1,10 +1,14 @@
 package bls12381
 
+import "fmt"
 import ffi "github.com/filecoin-project/filecoin-ffi"
+import "testing"
 
 func TestMapG2Filecoin(t *testing.T) {
 	msg := []byte("hello world")
 	msgFIL := ffi.Message(msg)
 	digestFIL := ffi.Hash(msgFIL)
-
+	res := MapToG2(msg, nil)
+	fmt.Println(digestFIL)
+	fmt.Println(res)
 }
